52a796ff9960eafbca08b7e2f1a7dc6fd01efaec
Thom719c <thom719c@stud.kea.dk>
2022-09-27 09:19:05 +0200

    commit

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
 
-@GetMapping("/")
-    public String index(){
-    return "index";
-}
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/page1")
+    public String page1() {
+        return "page1";
+    }
 }
